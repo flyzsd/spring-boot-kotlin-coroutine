7fe7cfbd1066d1d024bc0548f1e759be4205e5ac
Shudong Zhang <zhangshudong@gmail.com>
2020-06-06 21:44:46 +0800

    Add webclient with coroutine

---
+++ src/main/kotlin/com/shudong/spring/boot/kotlinapp/controller/KotlinController.kt
@@ -0,0 +1,32 @@
+package com.shudong.spring.boot.kotlinapp.controller
+
+import com.shudong.spring.boot.kotlinapp.service.KotlinService
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+private val logger: Logger = LoggerFactory.getLogger(KotlinController::class.java)
+
+data class Student(val name: String)
+
+@RestController
+@RequestMapping("/api")
+class KotlinController(
+    private val kotlinService: KotlinService
+) {
+
+    @GetMapping("/student")
+    fun fetchStudent(): Student {
+        val student = runBlocking {
+            logger.debug("+inside coroutine")
+            delay(1_000)
+            kotlinService.fetchStudent()
+        }
+        println("student -> $student")
+        return student
+    }
+}
---
+++ src/main/kotlin/com/shudong/spring/boot/kotlinapp/service/KotlinService.kt
@@ -0,0 +1,49 @@
+package com.shudong.spring.boot.kotlinapp.service
+
+import com.shudong.spring.boot.kotlinapp.controller.Student
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.http.MediaType
+import org.springframework.stereotype.Service
+import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.reactive.function.client.awaitBody
+
+private val logger: Logger = LoggerFactory.getLogger(KotlinService::class.java)
+
+data class Contributor(val login: String, val id: Long, val url: String, val contributions: Int)
+
+@Service
+class KotlinService(builder: WebClient.Builder) {
+    private val gitHubClient = builder.baseUrl("https://api.github.com").build()
+
+    suspend fun fetchStudent(): Student {
+        logger.debug("+fetchStudent")
+        delay(1_000)
+        logger.debug("-fetchStudent")
+
+        val contributors = coroutineScope {
+            val contributors1 = async {
+                fetchGithubRepoContributors("flyzsd", "spring-rest")
+            }
+            val contributors2 = async {
+                fetchGithubRepoContributors("flyzsd", "mjpeg")
+            }
+            contributors1.await() + contributors2.await()
+        }
+        logger.debug("contributors = $contributors")
+
+//        val contributors = fetchGithubRepoContributors("flyzsd", "spring-rest")
+//        logger.debug("contributors = $contributors")
+//        val contributors2 = fetchGithubRepoContributors("flyzsd", "mjpeg")
+//        logger.debug("contributors = $contributors2")
+
+        return Student("Jerry");
+    }
+
+    suspend fun fetchGithubRepoContributors(owner: String, repo: String): List<Contributor> {
+        return gitHubClient.get().uri("/repos/$owner/$repo/contributors").accept(MediaType.APPLICATION_JSON).retrieve().awaitBody()
+    }
+}
