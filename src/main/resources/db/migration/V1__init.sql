2fb6f8297748a722fb87abe86d5a1ab9fc830cb6
shudongzhang <zhangshudong@gmail.com>
2021-07-25 15:59:52 +0800

    Use mariadb for runtime
    - Run mariadb in docker

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS student
 (
     id          VARCHAR(255) NOT NULL PRIMARY KEY COMMENT 'uuid',
-    `name`        VARCHAR(255) NOT NULL,
+    `name`      VARCHAR(255) NOT NULL,
     modified_by VARCHAR(255) NOT NULL,
     modified_on TIMESTAMP    NOT NULL,
     version     BIGINT UNSIGNED
